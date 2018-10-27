5ba1676b136e39a00037a13f073894b5a088c821
Sieng Sotheara <sieng.sotheara.7@gmail.com>
2018-10-27 18:11:25 +0700

    change

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Layout from './Layout/Layout';
+import Home from './Home/Home';
+
+class App extends Component {
+  render() {
+    return (      
+      <Layout>
+        
+      </Layout>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route, Switch } from 'react-router-dom';
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('pageWrapper'));
