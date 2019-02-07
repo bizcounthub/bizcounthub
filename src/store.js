389f3e5774a197ae44e26f2f9419ce9b5b3741f2
Sotheara Sieng <sothearasieng@dhcp103.office1.digi.com.kh>
2019-02-07 21:41:34 +0700

    fix connected-router-redux

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
 import createHistory from 'history/createBrowserHistory';
-//import { connectRouter } from 'connected-react-router';
+import { connectRouter } from 'connected-react-router';
 
 import reducer from './reducer';
 export const history = createHistory();
-//export const store = createStore(connectRouter(history)(reducer));
-export const store = createStore(reducer);
+export const store = createStore(connectRouter(history)(reducer));
